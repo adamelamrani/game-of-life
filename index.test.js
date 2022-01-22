85e47a7ecfca6acb0e8be4cd08c05093dc1692ae
Adam El Amrani <adelamco1091@outlook.com>
2022-01-22 08:08:15 +0100

    deletes index.html, added final new lines

---
+++ index.test.js
@@ -0,0 +1,18 @@
+function generateArray(columns) {
+  const gameArrayColumns = [];
+}
+
+describe("Given a generateArray function", () => {
+  describe("When it receives columns", () => {
+    test("Then it should fail", () => {
+      const testValue = columns;
+      const expectedResult = 10;
+
+      const expectedFunctionResult = generateArray(testValue);
+
+      expect(expectedFunctionResult).toBe(expectedResult);
+    });
+  });
+
+  describe("When it reives");
+});
