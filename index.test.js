2d8bc638dcc8fe88722a6104db3763ac6bb0288b
Adam El Amrani <adelamco1091@outlook.com>
2022-01-23 10:54:45 +0100

    added index html

--- index.test.js
+++ index.test.js
@@ -23,11 +23,11 @@ describe("Given a generateArray function", () => {
     test("Then it should return an array of 1 * 1", () => {
       const testValue = 1;
       const testValue2 = 1;
-      const expectedResult = [[]];
+      const expectedResult = [[undefined]];
 
       const expectedFunctionResult = generateArray(testValue, testValue2);
 
-      expect(expectedFunctionResult).toStrictEqual(expectedResult);
+      expect(expectedFunctionResult).toBe(expectedResult);
     });
   });
 });
